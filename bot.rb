9bede31bf9daffd50535198a50db4c605b077386
Matthew Lavine <matt@matthewlavine.net>
2016-04-21 15:40:14 -0400

    The gun is now stateful and requires sequential actions to play.

--- bot.rb
+++ bot.rb
@@ -4,18 +4,6 @@ def action_string(string)
   "\001ACTION #{string}\001"
 end
 
-def roulette(m, nick)
-  if m.channel.has_user?(nick)
-    if Random.rand(1...7).modulo(6).zero?
-      m.reply action_string("shoots #{nick}.")
-    else
-      m.reply action_string("CLICK.")
-    end
-  else
-    m.reply "#{nick} is not online."
-  end
-end
-
 bot = Cinch::Bot.new do
     configure do |c|
         raise "Environment Variable for Server not Set." unless ENV.has_key?("server")
@@ -32,12 +20,53 @@ bot = Cinch::Bot.new do
         end
     end
 
+    magazine_size = 6
+    loaded = false
+    bullet = nil
+    position = nil
+
     on :channel, /^!rr$/ do |m|
-      roulette(m, m.user.nick)
+      m.reply "Russian Roulette: To play, !load the gun, !spin the chamber, then !pull the trigger."
     end
 
-    on :channel, /^!rr (.+)$/ do |m, nick|
-      roulette(m, nick.strip)
+    on :channel, /^!load$/ do |m|
+      if !loaded
+        m.reply "#{m.user.nick} loads the gun."
+        bullet = Random.rand(1...magazine_size + 1)
+        loaded = true
+      else
+        m.reply "The gun is already loaded."
+      end
+    end
+
+    on :channel, /^!spin$/ do |m|
+      if !loaded
+        m.reply "You must load the gun first!"
+      else
+        m.reply "#{m.user.nick} spins the drum."
+        position = Random.rand(1...magazine_size + 1)
+      end
+    end
+
+    on :channel, /^!pull$/ do |m|
+      if !loaded
+        m.reply "You must load the gun first!"
+      elsif position == nil
+          m.reply "You must spin the drum first!"
+      else
+        if bullet == position
+          m.reply action_string("shoots #{m.user.nick}.")
+          loaded = false
+        else
+          m.reply action_string("CLICK.")
+
+          if position == magazine_size
+            position = 0
+          else
+            position += 1
+          end
+        end
+      end
     end
 end
 
